36322e3abf6a1e3b0caa6c06775b2758f19b8dfd
Karthik Raja <karthikr2910@gmail.com>
2019-04-16 10:55:35 +0530

    Certain changes
    Signed-off-by: Karthik Raja <karthikr2910@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const user=0;
-const comp=0;
+let user=0;
+let comp=0;
 const user_span=document.getElementById("user_score");
 const comp_span=document.getElementById("comp_score");
 const score_div=document.querySelector(".score");
@@ -8,23 +8,59 @@ const r_div=document.getElementById("r");
 const p_div=document.getElementById("p");
 const c_div=document.getElementById("c");
 
-function g(choice){
-	console.log("pressed "+choice);
-	document.getElementById("user_score").textContent="1";
+
+function getComp(){
+		const choices = ['r','p','c'];
+		const randomNumber = Math.floor(Math.random()*3);
+		return choices[randomNumber];
+}
+
+function Win(){
+	user++;
+	user_span.innerHTML=user;
+}
+
+function Draw(){
+}
+
+function Lose(){
+	comp++;
+	comp_span.innerHTML=comp;
+}
+
+
+function game(choice){
+		const compChoice = getComp();
+		console.log("user "+choice);
+		console.log("comp "+compChoice);
+		if(choice==compChoice){
+			console.log("draw");
+			Draw();
+		}
+		if((choice=='r'&&compChoice=='p')||(choice=='p'&&compChoice=='c')||(choice=='c'&&compChoice=='r')){
+			console.log("Lose");
+			Lose();
+		}
+		if((choice=='r'&&compChoice=='c')||(choice=='p'&&compChoice=='r')||(choice=='c'&&compChoice=='p')){
+			console.log("Win");
+			Win();
+		}
+		
+		
 }
 
 
 function main(){
 r_div.addEventListener('click', function(){
-	g("r");
+	game("r");
 })
 
 p_div.addEventListener('click', function(){
-	g("p");
+	game("p");
 })
 
 c_div.addEventListener('click', function(){
-	g("c");
+	game("c");
 })
 }
 
