6daf481839d30ddf7de236e7df32cec8cd731b3f
Karthik Raja <karthikr2910@gmail.com>
2019-04-17 10:57:51 +0530

    Completed Styling to be done with etc functionalities
    Signed-off-by: Karthik Raja <karthikr2910@gmail.com>

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let comp=0;
 const user_span=document.getElementById("user_score");
 const comp_span=document.getElementById("comp_score");
 const score_div=document.querySelector(".score");
-const result_div=document.querySelector(".result");
+const result_div=document.querySelector(".result p");
 const r_div=document.getElementById("r");
 const p_div=document.getElementById("p");
 const c_div=document.getElementById("c");
@@ -15,19 +15,31 @@ function getComp(){
 		return choices[randomNumber];
 }
 
-function Win(){
+function Win(choice, compChoice){
 	user++;
 	user_span.innerHTML=user;
+	result_div.innerHTML=`${convertLetter(choice)} beats ${convertLetter(compChoice)} You Win!`;
+	document.getElementById(choice).classList.add('green');
+	setTimeout(()=>document.getElementById(choice).classList.remove('green'),1000);
 }
 
 function Draw(){
+	result_div.innerHTML="Its a Draw Baby!"
 }
 
-function Lose(){
+function Lose(choice, compChoice){
 	comp++;
 	comp_span.innerHTML=comp;
+	result_div.innerHTML=`${convertLetter(compChoice)} beats ${convertLetter(choice)}, You Lose!`;
+	document.getElementById(choice).classList.add('red');
+	setTimeout(()=>document.getElementById(choice).classList.remove('red'),1000);
 }
 
+function convertLetter(letter){
+		if(letter==="r") return "Rock";
+		if(letter==="p") return "Paper";
+		if(letter==="c") return "Scissor";
+}
 
 function game(choice){
 		const compChoice = getComp();
@@ -39,11 +51,11 @@ function game(choice){
 		}
 		if((choice=='r'&&compChoice=='p')||(choice=='p'&&compChoice=='c')||(choice=='c'&&compChoice=='r')){
 			console.log("Lose");
-			Lose();
+			Lose(choice, compChoice);
 		}
 		if((choice=='r'&&compChoice=='c')||(choice=='p'&&compChoice=='r')||(choice=='c'&&compChoice=='p')){
 			console.log("Win");
-			Win();
+			Win(choice, compChoice);
 		}
 		
 		
